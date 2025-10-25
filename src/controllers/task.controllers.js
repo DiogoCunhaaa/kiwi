7f8f57cb16e37b463bb0cc5cef891f51faadfb73
DiogoCunhaaa <diogo_f_cunha@estudante.sesisenai.org.br>
2025-10-25 12:03:49 -0300

    route editing backend

--- src/controllers/task.controllers.js
+++ src/controllers/task.controllers.js
@@ -1,4 +1,8 @@
-import { getAllTasks, insertTask, deleteTaskById } from "../models/task.models.js";
+import {
+  getAllTasks,
+  insertTask,
+  deleteTaskById,
+} from "../models/task.models.js";
 
 export const listTasks = async (req, res) => {
   try {
@@ -48,6 +52,10 @@ export const createTask = async (req, res) => {
 export const deleteTask = async (req, res) => {
   try {
     const { id } = req.params;
+
+    if (!id) {
+      return res.status(400).json({ error: "Preencha o id" });
+    }
     const affectedRows = await deleteTaskById(id);
 
     if (affectedRows == 0) {
@@ -55,6 +63,41 @@ export const deleteTask = async (req, res) => {
     }
 
     return res.status(200).json({ message: "Tarefa deletada com suceso" });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Erro no servidor" });
+  }
+};
+
+export const editTask = async (req, res) => {
+  try {
+    const { id } = req.params;
+    if (!id) {
+      return res.status(400).json({ error: "Preencha o id" });
+    }
+    const { descricao_tarefa, setor_tarefa, prioridade_tarefa, status_tarefa } = req.body;
+    if (
+      !descricao_tarefa ||
+      !setor_tarefa ||
+      !prioridade_tarefa ||
+      !status_tarefa
+    ) {
+      return res.status(400).json({ error: "Preencha todos os campos" });
+    }
+
+    const affectedRows = await editTaskById(id, {
+      descricao_tarefa,
+      setor_tarefa,
+      prioridade_tarefa,
+      status_tarefa,
+    });
+
+    if (affectedRows > 0) {
+      return res.status(200).json({ message: "Tarefa atualizada com sucesso" });
+    } else {
+      return res.status(404).json({ error: "Tarefa não encontrada" });
+    }
+
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: "Erro no servidor" });
--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { listTasks, createTask, deleteTask } from "../controllers/task.controllers.js";
+import { listTasks, createTask, deleteTask, editTask } from "../controllers/task.controllers.js";
 
 const router = Router();
 
 router.get("/", listTasks);
 router.post("/create", createTask);
 router.delete("/delete/:id", deleteTask);
+router.put("/edit/:id", editTask);
 
 export default router;
