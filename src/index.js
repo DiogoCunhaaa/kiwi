f66e38423f7e12dce5abf12d933c467312d15b55
DiogoCunhaaa <diogo_f_cunha@estudante.sesisenai.org.br>
2025-10-25 09:09:01 -0300

    database connection

--- src/index.js
+++ src/index.js
@@ -2,18 +2,6 @@ import express from "express";
 
 const app = express();
 
-app.use(
-  session({
-    secret: "SaepMan",
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-      maxAge: 1000 * 60 * 60,
-      secure: false,
-    },
-  })
-);
-
 app.use(express.json());
 app.use(express.static("./public"));
 
@@ -23,7 +11,7 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.listen(port, () => {
+app.listen(PORT, () => {
   console.log(`Backend running in http://localhost:${PORT}`);
 });
 
